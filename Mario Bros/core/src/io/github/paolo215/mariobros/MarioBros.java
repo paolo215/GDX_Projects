d89e03c0086140cbbae186e9e2fca72d43317b7e
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2016-08-27 16:41:34 -0700

    Mario Part 8

--- Mario Bros/core/src/io/github/paolo215/mariobros/MarioBros.java
+++ Mario Bros/core/src/io/github/paolo215/mariobros/MarioBros.java
@@ -13,6 +13,9 @@ public class MarioBros extends Game {
 	//virtual
 	public static final int V_WIDTH = 400;
 	public static final int V_HEIGHT = 208;
+
+	//pixels per meter
+	public static final float PPM = 100;
 	public SpriteBatch batch;
 	
 	@Override
--- Mario Bros/core/src/io/github/paolo215/mariobros/screens/PlayScreen.java
+++ Mario Bros/core/src/io/github/paolo215/mariobros/screens/PlayScreen.java
@@ -1,31 +1,202 @@
 package io.github.paolo215.mariobros.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import io.github.paolo215.mariobros.MarioBros;
 import io.github.paolo215.mariobros.scenes.Hud;
+import io.github.paolo215.mariobros.sprites.Mario;
 
 /**
  * Created by paolo on 8/26/2016.
  */
+
+/**
+ * world
+ *      world
+ *          bodies
+ *              mass
+ *              velocity
+ *              location
+ *              angles
+ *              fixtures
+ *                  shape
+ *                  density
+ *                  friction
+ *                  restitution
+ */
+
 public class PlayScreen implements Screen {
     private MarioBros game;
     private OrthographicCamera gamecam;
     private Hud hud;
     private Viewport gamePort;
 
+
+    //Tiled map
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    //Box2d
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
+    private Mario player;
+
+
     public PlayScreen(MarioBros game) {
         this.game = game;
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+        gamePort = new FitViewport(MarioBros.V_WIDTH / MarioBros.PPM,
+                MarioBros.V_HEIGHT / MarioBros.PPM, gamecam);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / MarioBros.PPM);
+        gamecam.position.set(gamePort.getWorldWidth() / 2,
+                gamePort.getWorldHeight() / 2, 0);
+
+        //first param: gravity, 2nd param: move (false) or at rest (true)
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+
+        //need to define what body consists of
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        //create ground bodies and fixtures
+        for(MapObject object : map.getLayers().get(2).getObjects()
+                .getByType(RectangleMapObject.class)) {
+
+            //body
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+            body = world.createBody(bdef);
+
+            //fixture
+            shape.setAsBox((rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getHeight() / 2) / MarioBros.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        //create pipe bodies/fixtures
+        for(MapObject object : map.getLayers().get(3).getObjects()
+                .getByType(RectangleMapObject.class)) {
+
+            //body
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+            body = world.createBody(bdef);
+
+            //fixture
+            shape.setAsBox((rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getHeight() / 2) / MarioBros.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+
+        //create brick bodies/fixtures
+        for(MapObject object : map.getLayers().get(5).getObjects()
+                .getByType(RectangleMapObject.class)) {
+
+            //body
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+            body = world.createBody(bdef);
+
+            //fixture
+            shape.setAsBox((rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getHeight() / 2) / MarioBros.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        //create coin bodies/fixtures
+        for(MapObject object : map.getLayers().get(4).getObjects()
+                .getByType(RectangleMapObject.class)) {
+
+            //body
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getY() + rect.getHeight() / 2) / MarioBros.PPM);
+            body = world.createBody(bdef);
+
+            //fixture
+            shape.setAsBox((rect.getWidth() / 2) / MarioBros.PPM,
+                    (rect.getHeight() / 2) / MarioBros.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        player = new Mario(world);
+
+    }
+
+    public void handleInput(float dt) {
+        //force = gradual movement
+        //impulse = immediate
+        if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            player.b2body.applyLinearImpulse(new Vector2(0, 4f),
+                    player.b2body.getWorldCenter(), true);
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)
+                && player.b2body.getLinearVelocity().x <= 2) {
+            player.b2body.applyLinearImpulse(new Vector2(0.1f, 0),
+                    player.b2body.getWorldCenter(), true);
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)
+                && player.b2body.getLinearVelocity().x >= -2) {
+            player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0),
+                    player.b2body.getWorldCenter(), true);
+        }
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+
+        world.step(1/60f, 6, 2);
+
+        gamecam.position.x = player.b2body.getPosition().x;
+
+        gamecam.update();
+
+        //tell our render to draw only what our camera can see in our game world
+        renderer.setView(gamecam);
     }
 
     @Override
@@ -35,11 +206,19 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        //separate our update logic from render
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //Recognize where camera is the gameworld and only render what cam can see
+
+        renderer.render();
+        b2dr.render(world, gamecam.combined);
+
+        //Recognize where camera is in the gameworld and only render what cam can see
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
+
     }
 
     @Override
