c7e428838a30748427f2646a3e47fb4568e3324d
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2016-08-29 18:10:32 -0700

    Part 13 Mario
    Mario is now able to "break" bricks whenever he hits it with his head.

--- Mario Bros/core/src/io/github/paolo215/mariobros/MarioBros.java
+++ Mario Bros/core/src/io/github/paolo215/mariobros/MarioBros.java
@@ -17,7 +17,14 @@ public class MarioBros extends Game {
 	//pixels per meter
 	public static final float PPM = 100;
 	public SpriteBatch batch;
-	
+
+	//filters
+	public static final short DEFAULT_BIT = 1;
+	public static final short MARIO_BIT = 2;
+	public static final short BRICK_BIT = 4;
+	public static final short COIN_BIT = 8;
+	public static final short DESTROYED_BIT = 16;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
--- Mario Bros/core/src/io/github/paolo215/mariobros/sprites/Brick.java
+++ Mario Bros/core/src/io/github/paolo215/mariobros/sprites/Brick.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 
+import io.github.paolo215.mariobros.MarioBros;
+
 /**
  * Created by paolo on 8/27/2016.
  */
@@ -12,10 +14,13 @@ public class Brick extends InteractiveTileObject {
     public Brick(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
         fixture.setUserData(this);
+        setCategoryFilter(MarioBros.BRICK_BIT);
     }
 
     @Override
     public void onHeadHit() {
         Gdx.app.log("Brick", "Brick collision");
+        setCategoryFilter(MarioBros.DESTROYED_BIT);
+        getCell().setTile(null);
     }
 }
